d2fe1fa1617a11c2d0112bcc0859b84b8022ffc9
Shuhad <shuhadz@hotmail.com>
2020-11-12 10:10:38 +0100

    dongle function

--- index.js
+++ index.js
@@ -1,17 +1,106 @@
-/* function randomNoGenerator(min, max) {
-  if(typeof(max) !== 'number' && typeof(min) !== 'number') {
-    min = 0;  max = 1;
-  }
- return (Math.random() * (max-min)) + min;
-}
-module.exports = randomNoGenerator; */
+let port;
+let reader;
+let inputDone;
+let outputDone;
+let inputStream;
+let outputStream;
+let isScanning = false;
 
-exports.ati = function() {
-  return 'device information'
-};
-exports.at_connect = function() {
+exports.at_connect = async function() {
+  if (port) {
+    if (isScanning) {
+      writeCmd("\x03");
+      isScanning = false;
+    }
+    await disconnect();
+    return;
+  }
+  await connect();
   return 'device connected'
 };
 exports.at_disconnect = function() {
+  await disconnect();
   return 'device disconnected'
 };
+
+exports.ati = function() {
+  writeCmd("ATI");
+  return 'device information'
+};
+
+async function connect() {
+  // - Request a port and open a connection.
+  port = await navigator.serial.requestPort();
+  // - Wait for the port to open.
+  await port.open({ baudRate: 9600 });
+
+  const encoder = new TextEncoderStream();
+  outputDone = encoder.readable.pipeTo(port.writable);
+  outputStream = encoder.writable;
+
+  let decoder = new TextDecoderStream();
+  inputDone = port.readable.pipeTo(decoder.writable);
+  inputStream = decoder.readable.pipeThrough(
+    new TransformStream(new LineBreakTransformer())
+  );
+
+  reader = inputStream.getReader();
+  readLoop().catch((error) => {
+    toggleUIConnected(false);
+    port = null;
+    log.textContent = "Dongle Disconnected!";
+  });
+}
+
+async function disconnect() {
+  // Close the input stream (reader).
+  if (reader) {
+    await reader.cancel();
+    await inputDone.catch(() => {});
+    reader = null;
+    inputDone = null;
+  }
+  // Close the output stream.
+  if (outputStream) {
+    await outputStream.getWriter().close();
+    await outputDone;
+    outputStream = null;
+    outputDone = null;
+  }
+  // Close the port.
+  await port.close();
+  port = null;
+  log.textContent = "Dongle Disconnected!";
+}
+
+function writeCmd(cmd) {
+  // Write to output stream
+  const writer = outputStream.getWriter();
+  console.log("[SEND]", cmd);
+  writer.write(cmd);
+  // Ignores sending carriage return if sending Ctrl+C
+  if (cmd !== "\x03") {
+    writer.write("\r"); // Important to send a carriage return after a command
+  }
+  writer.releaseLock();
+}
+
+class LineBreakTransformer {
+  constructor() {
+    // A container for holding stream data until a new line.
+    this.container = "";
+  }
+
+  transform(chunk, controller) {
+    // Handle incoming chunk
+    this.container += chunk;
+    const lines = this.container.split("\r\n");
+    this.container = lines.pop();
+    lines.forEach((line) => controller.enqueue(line));
+  }
+
+  flush(controller) {
+    // Flush the stream.
+    controller.enqueue(this.container);
+  }
+}
