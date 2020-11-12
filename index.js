c9bc7cab88b25f39186a295eb51a45de1e4235d1
Shuhad <shuhadz@hotmail.com>
2020-11-12 09:52:30 +0100

    test function added

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-function randomNoGenerator(min, max) {
+/* function randomNoGenerator(min, max) {
   if(typeof(max) !== 'number' && typeof(min) !== 'number') {
     min = 0;  max = 1;
   }
  return (Math.random() * (max-min)) + min;
 }
-module.exports = randomNoGenerator;
+module.exports = randomNoGenerator; */
+
+exports.ati = function() {
+  return 'device information'
+};
+exports.at_connect = function() {
+  return 'device connected'
+};
+exports.at_disconnect = function() {
+  return 'device disconnected'
+};
